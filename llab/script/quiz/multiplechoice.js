62d17b2520e60cec02199de770bc5fd01c3c1d95
Cinthia Rodriguez <cinthiaro@users.noreply.github.com>
2020-02-03 01:24:06 -0600

    Spanish translation V1

--- llab/script/quiz/multiplechoice.js
+++ llab/script/quiz/multiplechoice.js
@@ -122,7 +122,7 @@ MC.prototype.render = function() {
         //$('.MultipleChoice').html(pageTemplate);
 
         /* set the question type title */
-        this.multipleChoice.find('.questionType').html(/*'Question ' + (this.num + 1)*/ 'Self-Check Question'); /* This line edited by Mary Fries on 4/14/19 so that For You To Do problem numbers and quiz numbers don't conflict. */
+        this.multipleChoice.find('.questionType').html(/*'Question ' + (this.num + 1)*/ 'Pregunta de auto-evaluaci&oacute;n'); /* This line edited by Mary Fries on 4/14/19 so that For You To Do problem numbers and quiz numbers don't conflict. */
     }
 
     /* render the prompt */
